eb870a539f24c5cb90e6c1f7f28eff042824b041
ALUNO SESI SENAI HORTO <aluno@senaifiemg.com>
2023-03-02 22:05:10 -0300

    tanto faz

---
+++ scripts/funcoes/dialogo.js
@@ -0,0 +1,20 @@
+//Puxando a div tela
+const tela = document.querySelector('.tela');
+
+//Usar o link de HTML
+const getLink = function(complete){
+
+    return '<link rel="import" href="'+ complete +'">';
+
+}
+
+function exDialogo(){ 
+
+    
+
+
+    //Primeiramente, criar uma div onde o texto será exibido
+    
+
+
+}
