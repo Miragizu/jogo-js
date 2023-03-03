c5d29c1b3afe67ff6132b7d73c28ee1327104e61
Tales-MR <121312393+Tales-MR@users.noreply.github.com>
2023-03-03 14:24:20 -0300

    organização

--- scripts/funcoes/dialogo.js
+++ scripts/funcoes/dialogo.js
@@ -3,18 +3,10 @@ const tela = document.querySelector('.tela');
 
 //Usar o link de HTML
 const getLink = function(complete){
-
-    return '<link rel="import" href="'+ complete +'">';
-
+    return `link rel="import" href="${complete}>"`;
 }
 
 function exDialogo(){ 
 
     
-
-
-    //Primeiramente, criar uma div onde o texto será exibido
-    
-
-
 }
